3b260c915290dcf8b30c9a6dfcc804f65ac861e1
Rodo <pieroni.rodrigo@gmail.com>
2023-02-28 17:54:33 -0300

    second 16

--- js/app.js
+++ js/app.js
@@ -10,27 +10,64 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    const btnSubmit = document.querySelector('#formulario  button[type="submit"]')
+    const btnSubmit = document.querySelector('#formulario  button[type="submit"]');
+    const btnReset = document.querySelector('#formulario  button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
+
 
     //Asignar evento
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
     inputMensaje.addEventListener('blur', validar);
 
+    formulario.addEventListener('submit', enviarMail);
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+       resetearFormulario();
+    })
+
+    function enviarMail(e){
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            resetearFormulario();
+            //Crear un Alerta
+            const alertaExito = document.createElement('p');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p2', 'text-center', 'rounded-lg', 'mt-10',
+            'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent='Mensaje enviado con exito';
+            formulario.appendChild(alertaExito);
+            setInterval(() => {
+                alertaExito.remove();
+            }, 3000); 
+        }, 3000);
+        
+    }
+
+
     function validar(e) {
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         }
         if (e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta('El email no es valido: ', e.target.parentElement);
+            email[e.target.name] = ''
+            comprobarEmail();
             return;
         }
         limpiarAlerta(e.target.parentElement);
         //Asignar los valores
         email[e.target.name] = e.target.value.trim().toLowerCase();
         //comprobar el objeto del email
-        comprobarEmail();    }
+        comprobarEmail();
+    }
 
     function mostrarAlerta(mensaje, referencia) {
         limpiarAlerta(referencia);
@@ -55,11 +92,23 @@ document.addEventListener('DOMContentLoaded', function () {
         return resultado;
     }
 
-    function comprobarEmail(){
-       if(Object.values(email).includes('')){
-       }else{
+    function comprobarEmail() {
+        if (Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
-       }
     }
+
+    function resetearFormulario(){
+        //reiniciar el objeto
+        email.email='';
+        email.asunto='';
+        email.mensaje='';
+        formulario.reset();
+        comprobarEmail();
+    }
+
 });    
